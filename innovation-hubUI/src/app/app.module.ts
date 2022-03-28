72be711d6a69920940db29d1a8b0e985e0d520da
oksana.kulish <oksana.kulish@codeworldwide.com>
2022-03-28 15:49:34 +0300

    Add Material CDK

--- innovation-hubUI/src/app/app.module.ts
+++ innovation-hubUI/src/app/app.module.ts
@@ -5,14 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloService } from './web-api/services/hello.service';
+import { InputSliderComponent } from './shared/components/input-slider/input-slider.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputSliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [HttpClient, HelloService],
   bootstrap: [AppComponent]
---
+++ innovation-hubUI/src/app/shared/components/input-slider/input-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-slider',
+  templateUrl: './input-slider.component.html',
+  styleUrls: ['./input-slider.component.scss']
+})
+export class InputSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
