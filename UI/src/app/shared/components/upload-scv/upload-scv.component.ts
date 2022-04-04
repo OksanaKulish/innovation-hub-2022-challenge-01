50f216302d33a13715c5a9eabb70cce4feba4915
oksana.kulish <oksana.kulish@codeworldwide.com>
2022-04-05 02:45:08 +0300

    add grid

--- UI/src/app/shared/components/upload-scv/upload-scv.component.ts
+++ UI/src/app/shared/components/upload-scv/upload-scv.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { mergeMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { mergeMap, tap } from 'rxjs/operators';
+import { PredictService } from 'src/app/web-api/services/predict.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,33 +9,37 @@ import { environment } from 'src/environments/environment';
   templateUrl: './upload-scv.component.html',
   styleUrls: ['./upload-scv.component.scss'],
 })
-export class UploadScvComponent implements OnInit {
+export class UploadScvComponent {
   public fileName = '';
-  public url: any;
-  public constructor(private http: HttpClient) {}
+  public predictedData: any;
+  public isLoading = false;
 
-  public ngOnInit(): void {}
+  public constructor(private http: HttpClient,
+    private readonly predictService: PredictService) {}
 
-  public onUploadCSV(event: any) {
+  public async onUploadCSV(event: any) {
+
+
     const file: File = event.target.files[0];
 
     if (file) {
+      this.isLoading = true;
       this.fileName = file.name;
       const formData = new FormData();
       formData.append('csv', file);
-      const upload$ = this.http
-        .post(environment.uploadCSVUrl + 'dev/api/FileUpload', formData)
-        .pipe(
-          mergeMap((t: any) => {
-            console.log(
-              environment.apiUrl + 'getBulkValues?url=' + `${t.url}`
-            );
-            return this.http.get(
-              environment.apiUrl + 'getBulkValues?url=' + `${t.url}`
-            );
-          })
-        );
-      upload$.subscribe();
+      try {
+        const upload$ = this.http
+          .post(environment.uploadCSVUrl + 'dev/api/FileUpload', formData)
+          .pipe(
+            tap((x) => console.log(x)),
+            mergeMap(async (t: any) => {
+              return await this.predictService.getBulkValuesAsync(t);
+            })
+          );
+        upload$.subscribe((x) => (this.predictedData = x));
+      } finally {
+        this.isLoading = false;
+      }
     }
   }
 }
--- UI/src/app/web-api/services/predict.service.ts
+++ UI/src/app/web-api/services/predict.service.ts
@@ -14,4 +14,10 @@ export class PredictService extends WebApiBaseService {
       `getValue?Cylinders=${data.Cylinders}&Displacement=${data.Displacement}&Horsepower=${data.Horsepower}&Weight=${data.Weight}&Acceleration=${data.Acceleration}&Model+year=${data['Model year']}&Origin=${data.Origin}`
     );
   }
+
+  public async getBulkValuesAsync(data: any): Promise<string> {
+    return await this.getAsync(
+      `getBulkValues?url=${data.url}`
+    );
+  }
 }
