bab6a01ffcce88f080bc4c952a2d3068243f4f07
gilo_agilo <andrii.iegorov@gmail.com>
2022-04-20 17:45:44 +0300

    ADDED: images download into structure

--- Crawlers/src/main.py
+++ Crawlers/src/main.py
@@ -20,6 +20,14 @@ user_agent_list = [
 ]
 
 
+def get_models():
+    return ["focus", "c-max", "escape", "explorer", "mustang", "f-150", "fiesta", "kuga", "mondeo"]
+
+
+def get_brand():
+    return 'ford'
+
+
 def smart_truncate(src):
     return src[0:src.rfind('/')]
 
@@ -31,8 +39,10 @@ def get_user_agent():
     return headers
 
 
-def download_file(url):
+def download_file(url, folder=None):
     local_filename = url.split('/')[-1]
+    if os.path.exists(folder):
+        local_filename = os.path.join(folder, local_filename)
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True, headers=get_user_agent()) as r:
         r.raise_for_status()
@@ -51,153 +61,183 @@ def read_url(url):
         res = f.read()
     return res
 
-folders = ["data/visited/", "data/autos/"]
 
-for folder in folders:
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
-        print(folder, "created.")
-    else:
-        print(folder, "already existed")
+def download_images(car, folder):
+    brand = car['brand']
+    model = car['model']
+    color = car['color']
+    images = car['images']
+    import os
+    dir = os.path.join(folder, brand, model, color)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    images_counter = 0
+    for img in images:
+        from requests import HTTPError
+        try:
+            download_file(img, folder=dir)
+        except Exception as e:
+            print("Error: {}, Skipping file: {}", e, dir)
 
-path_to_visited_urls = "data/visited/visited_urls.json"
+        # print(".", end='')
+        images_counter += 1
+        # sleep(1)
+    # print("\n")
+    return images_counter
 
-if not os.path.isfile(path_to_visited_urls):
-    with open(path_to_visited_urls, "w") as file:
-        json.dump([], file)
 
-countries = {"Deutschland": "D",
-             "Oesterreich": "A",
-             "Belgien": "B",
-             "Spanien": "E",
-             "Frankreich": "F",
-             "Italien": "I",
-             "Luxemburg": "L",
-             "Niederlande": "NL"}
+def main():
+    folders = ["data/visited/", "data/autos/"]
 
-# countries = {"Deutschland": "D"}
+    for folder in folders:
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+            print(folder, "created.")
+        else:
+            print(folder, "already existed")
 
-brand = "ford"
-models = ["focus", "c-max", "escape", "explorer", "mustang", "f-150", "fiesta", "kuga", "mondeo"]
+    path_to_visited_urls = "data/visited/visited_urls.json"
 
-car_counter = 1
-cycle_counter = 0
+    if not os.path.isfile(path_to_visited_urls):
+        with open(path_to_visited_urls, "w") as file:
+            json.dump([], file)
 
-while True:
+    countries = {"Deutschland": "D",
+                 "Oesterreich": "A",
+                 "Belgien": "B",
+                 "Spanien": "E",
+                 "Frankreich": "F",
+                 "Italien": "I",
+                 "Luxemburg": "L",
+                 "Niederlande": "NL"}
 
-    with open(path_to_visited_urls) as file:
-        visited_urls = json.load(file)
+    # countries = {"Deutschland": "D"}
 
-    if len(visited_urls) > 100000:
-        visited_urls = []
+    brand = get_brand()
+    models = get_models()
 
-    multiple_cars_dict = {}
+    car_counter = 1
+    cycle_counter = 0
 
-    cycle_counter += 1
+    while True:
 
-    for country in countries:
-        for model in models:
-            car_URLs = []
+        with open(path_to_visited_urls) as file:
+            visited_urls = json.load(file)
 
-            for page in range(1, 21):
+        if len(visited_urls) > 100000:
+            visited_urls = []
 
-                try:
-                    url = 'https://www.autoscout24.de/lst/' + brand + '/' + model + '?sort=age&desc=1&ustate=N%2CU&size=20&page=' + str(
-                        page) + '&cy=' + countries[country] + '&atype=C&'
-                    only_a_tags = SoupStrainer("a")
-                    html = read_url(url)
-                    soup = BeautifulSoup(html, 'lxml', parse_only=only_a_tags)
-                except Exception as e:
-                    print("Übersicht: " + str(e) + " " * 50, end="\r")
-                    pass
+        multiple_cars_dict = {}
 
-                for link in soup.find_all("a"):
-                    if r"/angebote/" in str(link.get("href")):
-                        car_URLs.append(link.get("href"))
+        cycle_counter += 1
 
-                car_URLs_unique = [car for car in list(set(car_URLs)) if car not in visited_urls]
+        for country in countries:
+            for model in models:
+                car_URLs = []
 
-                print(f'Cycle {cycle_counter} | {country} | Page {page} | {len(car_URLs_unique)} new URLs', end="\r")
-            print("")
-            if len(car_URLs_unique) > 0:
+                for page in range(1, 21):
 
-                for URL in car_URLs_unique:
-                    print(f'Cycle {cycle_counter} | {country} | Auto {car_counter}' + ' ' * 50, end="\r")
                     try:
-                        car_counter += 1
-
-                        car_dict = {}
-                        car_dict["country"] = country
-                        car_dict["date"] = str(datetime.now())
-                        car = BeautifulSoup(read_url('https://www.autoscout24.de' + URL), 'lxml')
-
-                        for key, value in zip(car.find_all("dt"), car.find_all("dd")):
-                            car_dict[key.text.replace("\n", "")] = value.text.replace("\n", "")
-
-                        img_count = 1
-                        # for gallery in car.find_all("div", attrs={"class":"image-gallery-slides"}):
-                        #     for pics in gallery.find_all("picture", attrs={"class":"css-1uafc8p e3j2jx20"}):
-                        #         for img in pics.find_all("img"):
-                        #             res = img
-                        #             car_dict["img_{0:2d}".format(img_count)] = res['src']
-                        #             img_count += 1
-
-                        car_images = []
-                        for thumbnails in car.find_all("div", attrs={"class", "image-gallery-thumbnails-container"}):
-                            for img in thumbnails.find_all("img", attrs={"class", "image-gallery-thumbnail-image"}):
-                                car_images.append(smart_truncate(img['src']))
-                                img_count += 1
-                        car_dict["images"] = car_images
-
-                        if len(car_images) < 1:
-                            # no images - no fun
-                            continue
-
-                        car_dict["haendler"] = car.find("div", attrs={"class": "cldt-vendor-contact-box",
-                                                                      "data-vendor-type": "dealer"}) != None
-
-                        car_dict["privat"] = car.find("div", attrs={"class": "cldt-vendor-contact-box",
-                                                                    "data-vendor-type": "privateseller"}) != None
-
-                        # car_dict[""]
-                        # car_dict["ort"] = car.find("div", attrs={"class": "sc-grid-col-12",
-                        #                                          "data-item-name": "vendor-contact-city"}).text
-
-                        # car_dict["price"] = "".join(
-                        #     re.findall(r'[0-9]+', car.find("div", attrs={"class": "cldt-price"}).text))
-
-                        ausstattung = []
-
-                        for i in car.find_all("div", attrs={
-                            "class": "cldt-equipment-block sc-grid-col-3 sc-grid-col-m-4 sc-grid-col-s-12 sc-pull-left"}):
-                            for span in i.find_all("span"):
-                                ausstattung.append(i.text)
-
-                        ausstattung2 = []
-
-                        for element in list(set(ausstattung)):
-                            austattung_liste = element.split("\n")
-                            ausstattung2.extend(austattung_liste)
-
-                        car_dict["ausstattung_liste"] = sorted(list(set(ausstattung2)))
-
-                        multiple_cars_dict[URL] = car_dict
-                        visited_urls.append(URL)
+                        url = 'https://www.autoscout24.de/lst/' + brand + '/' + model + '?sort=age&desc=1&ustate=N%2CU&size=20&page=' + str(
+                            page) + '&cy=' + countries[country] + '&atype=C&'
+                        only_a_tags = SoupStrainer("a")
+                        html = read_url(url)
+                        soup = BeautifulSoup(html, 'lxml', parse_only=only_a_tags)
                     except Exception as e:
-                        print("Detail page: " + str(e) + " " * 50)
+                        print("Übersicht: " + str(e) + " " * 50, end="\r")
                         pass
+
+                    for link in soup.find_all("a"):
+                        if r"/angebote/" in str(link.get("href")):
+                            car_URLs.append(link.get("href"))
+
+                    car_URLs_unique = [car for car in list(set(car_URLs)) if car not in visited_urls]
+
+                    print(f'Cycle {cycle_counter} | {country} | Page {page} | {len(car_URLs_unique)} new URLs', end="\r")
                 print("")
+                if len(car_URLs_unique) > 0:
 
-            else:
-                print("\U0001F634")
-                sleep(60)
+                    for URL in car_URLs_unique:
+                        print(f'Cycle {cycle_counter} | {country} | Auto {car_counter}' + ' ' * 50, end="\r")
+                        try:
+                            car_counter += 1
 
-    if len(multiple_cars_dict) > 0:
-        # df = pd.DataFrame(multiple_cars_dict).T
-        # df.to_csv("data/autos/" + re.sub("[.,:,-, ]", "_", str(datetime.now())) + ".csv", sep=";", index_label="url")
-        with open("data/autos/multiple_cars_dict_" + re.sub("[.,:,-, ]", "_", str(datetime.now())) + ".json", "w") as file:
-            json.dump(multiple_cars_dict, file)
-    else:
-        print("No data")
-    with open("data/visited/visited_urls.json", "w") as file:
-        json.dump(visited_urls, file)
+                            car_dict = {}
+                            car_dict["country"] = country
+                            car_dict["date"] = str(datetime.now())
+                            car = BeautifulSoup(read_url('https://www.autoscout24.de' + URL), 'lxml')
+
+                            for key, value in zip(car.find_all("dt"), car.find_all("dd")):
+                                car_dict[key.text.replace("\n", "")] = value.text.replace("\n", "")
+
+                            img_count = 1
+                            # for gallery in car.find_all("div", attrs={"class":"image-gallery-slides"}):
+                            #     for pics in gallery.find_all("picture", attrs={"class":"css-1uafc8p e3j2jx20"}):
+                            #         for img in pics.find_all("img"):
+                            #             res = img
+                            #             car_dict["img_{0:2d}".format(img_count)] = res['src']
+                            #             img_count += 1
+
+                            car_images = []
+                            for thumbnails in car.find_all("div", attrs={"class", "image-gallery-thumbnails-container"}):
+                                for img in thumbnails.find_all("img", attrs={"class", "image-gallery-thumbnail-image"}):
+                                    car_images.append(smart_truncate(img['src']))
+                                    img_count += 1
+                            car_dict["images"] = car_images
+
+                            if len(car_images) < 1:
+                                # no images - no fun
+                                continue
+
+                            car_dict["haendler"] = car.find("div", attrs={"class": "cldt-vendor-contact-box",
+                                                                          "data-vendor-type": "dealer"}) != None
+
+                            car_dict["privat"] = car.find("div", attrs={"class": "cldt-vendor-contact-box",
+                                                                        "data-vendor-type": "privateseller"}) != None
+
+                            # car_dict[""]
+                            # car_dict["ort"] = car.find("div", attrs={"class": "sc-grid-col-12",
+                            #                                          "data-item-name": "vendor-contact-city"}).text
+
+                            # car_dict["price"] = "".join(
+                            #     re.findall(r'[0-9]+', car.find("div", attrs={"class": "cldt-price"}).text))
+
+                            ausstattung = []
+
+                            for i in car.find_all("div", attrs={
+                                "class": "cldt-equipment-block sc-grid-col-3 sc-grid-col-m-4 sc-grid-col-s-12 sc-pull-left"}):
+                                for span in i.find_all("span"):
+                                    ausstattung.append(i.text)
+
+                            ausstattung2 = []
+
+                            for element in list(set(ausstattung)):
+                                austattung_liste = element.split("\n")
+                                ausstattung2.extend(austattung_liste)
+
+                            car_dict["ausstattung_liste"] = sorted(list(set(ausstattung2)))
+
+                            multiple_cars_dict[URL] = car_dict
+                            visited_urls.append(URL)
+                        except Exception as e:
+                            print("Detail page: " + str(e) + " " * 50)
+                            pass
+                    print("")
+
+                else:
+                    print("\U0001F634")
+                    sleep(60)
+
+        if len(multiple_cars_dict) > 0:
+            # df = pd.DataFrame(multiple_cars_dict).T
+            # df.to_csv("data/autos/" + re.sub("[.,:,-, ]", "_", str(datetime.now())) + ".csv", sep=";", index_label="url")
+            with open("data/autos/multiple_cars_dict_" + re.sub("[.,:,-, ]", "_", str(datetime.now())) + ".json", "w") as file:
+                json.dump(multiple_cars_dict, file)
+        else:
+            print("No data")
+        with open("data/visited/visited_urls.json", "w") as file:
+            json.dump(visited_urls, file)
+
+
+if __name__ == "__main__":
+    main()
--- Crawlers/src/test_soup.py
+++ Crawlers/src/test_soup.py
@@ -1,21 +1,5 @@
-import urllib
-import urllib.request  # aufrufen von URLs
-import pytest
-
-def download_file(url):
-    local_filename = url.split('/')[-1]
-    # NOTE the stream=True parameter below
-    import requests
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk:
-                f.write(chunk)
-    return local_filename
-
+import json
+from main import download_file, smart_truncate, get_models, get_brand, download_images
 
 
 def test_a():
@@ -30,10 +14,98 @@ def test_a():
     print(res)
 
 
-def smart_truncate(src):
-    return src[0:src.rfind('/')]
-
 def test_b():
     url1 = 'https://aaa.com/abcd.jpg/120x33.jpg'
-
     assert 'https://aaa.com/abcd.jpg' == smart_truncate(url1)
+
+
+def build_prefixes():
+    prefixes = list()
+    brand = get_brand()
+    for m in get_models():
+        prefix = '/angebote/' + get_brand() + '-' + m + '-'
+        p = {
+            'prefix': prefix,
+            'brand': brand,
+            'model': m
+        }
+        prefixes.append(p)
+    return prefixes
+
+
+def discover_model(u):
+    idx = u.find(get_brand()) + len(get_brand()) + 1
+    idx2 = u[idx:].find('-', 2)
+    return u[idx:idx+idx2]
+
+
+def test_extract_model():
+    urls = ['/angebote/ford-focus-turnier-1-0-benzin-rot-c0f24c63-59e1-4455-b79a-1c662ab362fd',
+            '/angebote/ford-focus-cool-connect-1-0l-ecoboost-led-navi-pdc-elektro-benzin-grau-1cd71482-667f-411b-9ae3-0f09a2d19a4e',
+            '/angebote/ford-c-max-titanium-xenon-navi-kamera-panoramadach-benzin-grau-e6df043b-a74f-458c-8003-620a50489248',
+            '/angebote/ford-c-max-1-0-ecoboost-start-stopp-system-sync-edition-benzin-grau-80be8aad-4632-4c89-87a4-9e7daec69cbe',
+            '/angebote/ford-c-max-2-0-titanium-klima-navi-ahzv-pano-pdc-diesel-schwarz-353df5bd-ea2b-43de-9752-e89d7700abe8',
+            '/angebote/ford-explorer-plug-in-hybrid-st-line-4x4-elektro-benzin-blau-a1c5154d-347e-4a20-9f5f-49af5aacf7d0'
+            ]
+
+    prefixes = build_prefixes()
+    # pair {brand, model} => prefix => substr (^prefix-)
+    for u in urls:
+        print(discover_model(u))
+
+
+def test_d():
+    import glob
+    json_files_list = glob.glob('../data/autos/multiple_cars_dict_*.json')
+    colors = set()
+
+    color_map = {
+        'Blau': 'Blue',
+        'Braun': 'Brown',
+        'Sonstige': 'Other',
+        'Grau': 'Grey',
+        'Weiß': 'White',
+        'Metallic': 'Metallic',
+        'Violett': 'Purple',
+        'Schwarz': 'Black',
+        'Silber': 'Silver',
+        'Rot': 'Red',
+        'Bronze': 'Bronze',
+        'Orange': 'Orange',
+        'Beige': 'Beige',
+        'Gold': 'Gold',
+        'Gelb': 'Yellow',
+        'Grün': 'Green'}
+
+    brand = get_brand()
+    car_counter = 0
+    car_images = 0
+
+    for fn in json_files_list:
+        with open(fn, "r") as f:
+            res = json.load(f)
+
+        # print(res)
+        for k, v in res.items():
+            model = discover_model(k)
+            color = 'Unknown'
+            if 'Außenfarbe' in v:
+                de_color = v['Außenfarbe']
+                colors.add(de_color)
+                if de_color in color_map:
+                    color = color_map[de_color]
+            v['color'] = color
+            v['model'] = model
+            v['brand'] = brand
+            car_images += download_images(v, folder="..")
+            car_counter += 1
+
+            if car_counter % 20 == 0:
+                print("Cars: {0:05d}, Car images: {1:05d}", car_counter, car_images)
+                print("Pausing for 180 seconds...")
+                from time import sleep
+                sleep(180)
+
+            # break
+    # print(colors)
+    print(car_counter)
